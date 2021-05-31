8048fe827e6cc478ad8683e2889ecbca1adc8f76
Deepak Gautam <dipakgautam120@gmail.com>
2021-05-31 20:28:35 +0200

    timeout to toastr

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,25 +1,25 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators,
-} from "@angular/forms";
-import { EntryService } from "../../services/entry.service";
-import { ToastrService } from "ngx-toastr";
-import { Router } from "@angular/router";
-import { Entrycode } from "../../models/entrycode";
+} from '@angular/forms';
+import { EntryService } from '../../services/entry.service';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { Entrycode } from '../../models/entrycode';
 
 @Component({
-  selector: "app-home",
-  templateUrl: "./home.component.html",
-  styleUrls: ["./home.component.scss"],
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
   @Input() formValue = [];
   users: Entrycode[] = [];
   isLoading: boolean = true;
-  errorMessage: string = "";
+  errorMessage: string = '';
   isLoggedIn: boolean = false;
   form: FormGroup;
 
@@ -31,7 +31,7 @@ export class HomeComponent implements OnInit {
   ) {}
   ngOnInit() {
     this.form = this.fb.group({
-      code: ["", Validators.required],
+      code: ['', Validators.required],
     });
     this.getUsers();
   }
@@ -41,7 +41,7 @@ export class HomeComponent implements OnInit {
         this.users = users;
       },
       (error) => {
-        console.error("error caught on subscribing user data");
+        console.error('error caught on subscribing user data');
         this.errorMessage = error;
         this.isLoading = false;
       }
@@ -50,17 +50,19 @@ export class HomeComponent implements OnInit {
 
   onBtnCLick(): void {
     var result = this.users.find(
-      (code) => code.code === this.form.controls["code"].value
+      (code) => code.code === this.form.controls['code'].value
     );
 
     if (result) {
       this._entryService.isLoggedIn = true;
-      this.toastr.success("Welcome to Dashboard", "Success!", {
-        timeOut: 1500,
+      this.toastr.success('Welcome to Dashboard', 'Success!', {
+        timeOut: 1400,
       });
-      this.router.navigate(["dashboard"]);
+      this.router.navigate(['dashboard']);
     } else {
-      this.toastr.error("Code didn't match. Try Again");
+      this.toastr.error('Try Again', "Code didn't match", {
+        timeOut: 1400,
+      });
     }
   }
 }
